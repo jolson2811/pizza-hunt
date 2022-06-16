20214e949e9832cd2b70fed92d550907fdd278d5
jolson2811 <jolson2811@gmail.com>
2022-06-15 21:17:21 -0600

    added comment view and reply capabilities

--- routes/api/comment-routes.js
+++ routes/api/comment-routes.js
@@ -2,10 +2,15 @@ const router = require("express").Router();
 const {
   addComment,
   removeComment,
+  addReply,
+  removeReply,
 } = require("../../controllers/comment-controller");
 
 // /api/comments/<pizzaId>
-router.route("/:pizzaId").post(addComment);
+router.route("/:pizzaId/:commentId").put(addReply).delete(removeComment);
+
+// Delete route / remove reply
+router.route("/:pizzaId/:commentId/:replyId").delete(removeReply);
 
 // /api/comments/<pizzaId>/<commentId>
 router.route("/:pizzaId/:commentId").delete(removeComment);
